a68b78cfd18f847f1c621038f1723ca7698e71e9
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-06-29 20:00:34 +0530

    panda

---
+++ pandas/pandaDict16.py
@@ -0,0 +1,10 @@
+import pandas as p
+d={'one':p.Series([1,2,5],index=['a','b','c']),'two':p.Series([1,9,3],index=['a','b','c'])}
+data=p.DataFrame(d)
+print(data)
+print("SUM : \n",data.sum())
+print("MIN :\n",data.min())
+print("MAX :\n",data.max())
+print("MIN INDEX :\n",data.idxmin())
+print("MAX INDEX :\n",data.idxmax())
+print(data.describe())
