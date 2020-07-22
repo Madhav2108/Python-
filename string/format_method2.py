38fbb673589d6a76a6048a7507db747af929e55c
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-22 20:48:28 +0530

    Add files via upload

---
+++ string/format_method2.py
@@ -0,0 +1,8 @@
+# integer arguments
+print("The number is:{:d}".format(123))
+
+# float arguments
+print("The float number is:{:f}".format(123.4567898))
+
+# octal, binary and hexadecimal format
+print("bin: {0:b}, oct: {0:o}, hex: {0:x}".format(12))
