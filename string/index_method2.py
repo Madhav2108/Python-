632044198a916b94c79469a86f2ba6992994b913
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-07-28 11:29:35 +0530

    Add files via upload

---
+++ string/index_method2.py
@@ -0,0 +1,10 @@
+sentence = 'Python programming is fun.'
+
+# Substring is searched in 'gramming is fun.'
+print(sentence.index('ing', 10))
+
+# Substring is searched in 'gramming is '
+print(sentence.index('g is', 10, -4))
+
+# Substring is searched in 'programming'
+print(sentence.index('fun', 7, 18))
