b2a19b75c84406e4c074065806232910995c5726
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-02 19:44:09 +0530

    Add files via upload

---
+++ string/ljust_method.py
@@ -0,0 +1,14 @@
+# example string
+string = 'cat'
+width = 5
+
+# print left justified string
+print(string.ljust(width))
+
+# example string
+string = 'cat'
+width = 5
+fillchar = '*'
+
+# print left justified string
+print(string.ljust(width, fillchar))
