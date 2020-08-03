3206bb84e20cc926a5fa292a25ed46844d10fd28
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-03 22:19:27 +0530

    Add files via upload

---
+++ string/rjust_method.py
@@ -0,0 +1,14 @@
+# example string
+string = 'cat'
+width = 5
+
+# print right justified string
+print(string.rjust(width))
+
+# example string
+string = 'cat'
+width = 5
+fillchar = '*'
+
+# print right justified string
+print(string.rjust(width, fillchar))
