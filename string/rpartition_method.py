dbfd00ebba87e93db3289c0bf5f58a32af998f6f
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-08-03 22:20:12 +0530

    Add files via upload

---
+++ string/rpartition_method.py
@@ -0,0 +1,12 @@
+string = "Python is fun"
+
+# 'is' separator is found
+print(string.rpartition('is '))
+
+# 'not' separator is not found
+print(string.rpartition('not '))
+
+string = "Python is fun, isn't it"
+
+# splits at last occurence of 'is'
+print(string.rpartition('is'))
