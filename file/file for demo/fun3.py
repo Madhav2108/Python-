827a2acf088c21ea97a5ea59aa114bd93634bdec
Madhav Narayan <madhavnarayanwebd@gmail.com>
2020-11-07 21:16:39 +0530

    Add files via upload

---
+++ file/file for demo/fun3.py
@@ -0,0 +1,15 @@
+#functions
+#default value
+def dis(a=5,b=7):
+   print(a+b)
+dis()
+dis(4)
+dis(9)
+dis(4,9)
+#error
+#def dis(a,b=7,c):     #invalid
+#def dis(a=4,b,c):     #valid
+#def dis(a,b,c):       #valid
+#def dis(a=5,b=3,c):   #valid
+#def dis(a=5,3,c=4):   #invalid
+#def dis(5,a=3,c=3):   #invalid
